c13c9131f7b17ce2b24cc7ee9ba65d51bef93cf9
raito95 <raitochiu@jorjin.com.tw>
2023-09-05 18:01:35 +0800

    Updated reflections 3

--- src/ReflectionsDB.js
+++ src/ReflectionsDB.js
@@ -11,6 +11,12 @@ const reflections = [
         description: 'We\'ve known each other for so long and I don\'t even know your name.',
         imageUrl: '/react-test/2.webp'
     },
+    {
+        id: 3,
+        title: 'Cat diary ep2',
+        description: 'Everywhere the sun rises in the morning.',
+        imageUrl: '/react-test/3.webp'
+    }
   ];
   
   export default reflections;
